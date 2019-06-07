7e37aad53dcb3e8c990b779841963ec88b459902
Altelus <altelusenyodelus@gmail.com>
2019-06-07 09:56:00 +0800

    Add files via upload

--- controllers/Something.php
+++ controllers/Something.php
@@ -3,8 +3,31 @@
 class Something extends CI_Controller {
 
 
-	public class _g
+	public function __construct(){
+		parent::__construct();
+		$this->load->model('news_model');
+		$this->load->helper('url_helper');
+	}
 
+	public function index(){
+
+		$data['api'] = "
+			{info : 'ABout something',
+			 specifics : 'Nothing',
+			 filler : '0',
+			}
+		";
+		
+		$this->load->view("something/index", $data);
+	}
+
+
+
+
+	public function api(){
+		$data['api'] = json_encode($this->input->post(NULL,TRUE));
+		$this->load->view("something/api", $data);
+	}
 }
 
 
